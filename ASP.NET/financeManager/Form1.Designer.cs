3c4f4261cb718f7bfda4b15b4a3f686c68b959a8
Matteo Bernardi <matteo.bernardi.005@gmail.com>
2024-04-17 18:43:05 +0200

    Create interfate + Save data on txt

--- ASP.NET/financeManager/Form1.Designer.cs
+++ ASP.NET/financeManager/Form1.Designer.cs
@@ -28,27 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.inputValue = new System.Windows.Forms.TextBox();
             this.inputType = new System.Windows.Forms.ComboBox();
             this.inputMonth = new System.Windows.Forms.ComboBox();
+            this.addItem = new System.Windows.Forms.Button();
+            this.missingData = new System.Windows.Forms.Label();
+            this.inputName = new financeManager.PlaceHolderTextBox();
+            this.inputValue = new financeManager.PlaceHolderTextBox();
             this.SuspendLayout();
-            // 
-            // inputValue
-            // 
-            this.inputValue.Location = new System.Drawing.Point(400, 12);
-            this.inputValue.Name = "inputValue";
-            this.inputValue.Size = new System.Drawing.Size(298, 20);
-            this.inputValue.TabIndex = 0;
-            this.inputValue.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // inputType
             // 
             this.inputType.FormattingEnabled = true;
             this.inputType.Items.AddRange(new object[] {
-            "Entrata",
-            "Uscita",
-            "Abbonamento"});
-            this.inputType.Location = new System.Drawing.Point(400, 47);
+            "Entrance",
+            "Expence",
+            "Subscription"});
+            this.inputType.Location = new System.Drawing.Point(329, 47);
             this.inputType.Name = "inputType";
             this.inputType.Size = new System.Drawing.Size(134, 21);
             this.inputType.TabIndex = 1;
@@ -58,44 +53,94 @@
             // 
             this.inputMonth.FormattingEnabled = true;
             this.inputMonth.Items.AddRange(new object[] {
-            "Gennaio",
-            "Febbraio",
-            "Marzo",
-            "Aprile",
-            "Maggio",
-            "Giugno",
-            "Luglio",
-            "Agosto",
-            "Settembre",
-            "Ottobre",
-            "Novembre",
-            "Dicembre"});
-            this.inputMonth.Location = new System.Drawing.Point(564, 47);
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"});
+            this.inputMonth.Location = new System.Drawing.Point(493, 47);
             this.inputMonth.Name = "inputMonth";
             this.inputMonth.Size = new System.Drawing.Size(134, 21);
             this.inputMonth.TabIndex = 2;
             this.inputMonth.SelectedIndexChanged += new System.EventHandler(this.inputMonth_SelectedIndexChanged);
+            // 
+            // addItem
+            // 
+            this.addItem.Location = new System.Drawing.Point(666, 12);
+            this.addItem.Name = "addItem";
+            this.addItem.Size = new System.Drawing.Size(90, 20);
+            this.addItem.TabIndex = 3;
+            this.addItem.Text = "Add";
+            this.addItem.UseVisualStyleBackColor = true;
+            this.addItem.Click += new System.EventHandler(this.addItem_Click);
+            // 
+            // missingData
+            // 
+            this.missingData.AutoSize = true;
+            this.missingData.BackColor = System.Drawing.Color.Crimson;
+            this.missingData.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.missingData.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.missingData.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.missingData.Location = new System.Drawing.Point(648, 49);
+            this.missingData.Name = "missingData";
+            this.missingData.Size = new System.Drawing.Size(108, 19);
+            this.missingData.TabIndex = 4;
+            this.missingData.Text = "Missing Data";
+            this.missingData.Visible = false;
+            this.missingData.Click += new System.EventHandler(this.missingData_Click);
+            // 
+            // inputName
+            // 
+            this.inputName.Location = new System.Drawing.Point(329, 12);
+            this.inputName.Name = "inputName";
+            this.inputName.PlaceHolderText = null;
+            this.inputName.Size = new System.Drawing.Size(134, 20);
+            this.inputName.TabIndex = 5;
+            this.inputName.Text = "Name";
+            // 
+            // inputValue
+            // 
+            this.inputValue.Location = new System.Drawing.Point(493, 12);
+            this.inputValue.Name = "inputValue";
+            this.inputValue.PlaceHolderText = null;
+            this.inputValue.Size = new System.Drawing.Size(134, 20);
+            this.inputValue.TabIndex = 6;
+            this.inputValue.Text = "Value";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1061, 650);
+            this.Controls.Add(this.inputValue);
+            this.Controls.Add(this.inputName);
+            this.Controls.Add(this.missingData);
+            this.Controls.Add(this.addItem);
             this.Controls.Add(this.inputMonth);
             this.Controls.Add(this.inputType);
-            this.Controls.Add(this.inputValue);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox inputValue;
         private System.Windows.Forms.ComboBox inputType;
         private System.Windows.Forms.ComboBox inputMonth;
+        private System.Windows.Forms.Button addItem;
+        private System.Windows.Forms.Label missingData;
+        private PlaceHolderTextBox inputName;
+        private PlaceHolderTextBox inputValue;
     }
 }
 
--- ASP.NET/financeManager/Form1.cs
+++ ASP.NET/financeManager/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,25 @@ namespace financeManager
         public Form1()
         {
             InitializeComponent();
+            //PlaceHolders
+          
+
+            //Data File check and creation
+
+            string fileName = "sheet.txt"; // The name of the file you want to check
+            string directoryPath = Directory.GetCurrentDirectory(); // Get the current directory
+
+            string filePath = Path.Combine(directoryPath, fileName); // Combine directory path and file name
+
+            if (File.Exists(filePath))
+            {
+                Console.WriteLine($"File {fileName} exists in the directory.");
+            }
+            else
+            {
+                Console.WriteLine($"File {fileName} does not exist in the directory.");
+                File.Create( filePath );
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -30,6 +50,108 @@ namespace financeManager
         private void inputMonth_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void addItem_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(inputValue.Text, out double Value) && !string.IsNullOrEmpty(inputType.Text) && !string.IsNullOrEmpty(inputMonth.Text) && !string.IsNullOrEmpty(inputName.Text))
+            {
+                Console.WriteLine("DATI OK");
+                string Name = inputName.Text;
+                string Type = inputType.Text; 
+                string Month = inputMonth.Text;
+
+                string data = ($"{Name},{Value},{Type},{Month}");
+
+
+                using (StreamWriter w = File.AppendText("sheet.txt"))
+                {
+                    w.WriteLine(data);
+                }
+
+                Console.WriteLine($"{Name} {Value} {Type} {Month}");
+                missingData.Visible = false;
+            }
+            else
+            {
+                missingData.Visible = true;
+                Console.WriteLine("DATI MANCANTI");
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void missingData_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    // Placeholders
+
+    public class PlaceHolderTextBox : TextBox
+    {
+
+        bool isPlaceHolder = true;
+        string _placeHolderText;
+        public string PlaceHolderText
+        {
+            get { return _placeHolderText; }
+            set
+            {
+                _placeHolderText = value;
+                setPlaceholder();
+            }
+        }
+
+        public new string Text
+        {
+            get => isPlaceHolder ? string.Empty : base.Text;
+            set => base.Text = value;
+        }
+
+        //when the control loses focus, the placeholder is shown
+        private void setPlaceholder()
+        {
+            if (string.IsNullOrEmpty(base.Text))
+            {
+                base.Text = PlaceHolderText;
+                this.ForeColor = Color.Gray;
+                this.Font = new Font(this.Font, FontStyle.Italic);
+                isPlaceHolder = true;
+            }
+        }
+
+        //when the control is focused, the placeholder is removed
+        private void removePlaceHolder()
+        {
+
+            if (isPlaceHolder)
+            {
+                base.Text = "";
+                this.ForeColor = System.Drawing.SystemColors.WindowText;
+                this.Font = new Font(this.Font, FontStyle.Regular);
+                isPlaceHolder = false;
+            }
+        }
+        public PlaceHolderTextBox()
+        {
+            GotFocus += removePlaceHolder;
+            LostFocus += setPlaceholder;
+        }
+
+        private void setPlaceholder(object sender, EventArgs e)
+        {
+            setPlaceholder();
+        }
+
+        private void removePlaceHolder(object sender, EventArgs e)
+        {
+            removePlaceHolder();
         }
     }
 }
